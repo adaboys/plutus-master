0a94dee2338f65529a61b421faba08167b4becbd
ADABoys LAB <94250743+adaboys@users.noreply.github.com>
2021-12-31 11:44:15 +0700

    Plutus.hs
    Plutus.hs

---
+++ src/Plutus.hs
@@ -0,0 +1,20 @@
+module Plutus
+    ( module Control.Monad
+    , module Optics
+    , module Plutus.Hash
+    , module Plutus.Types
+    , module Plutus.Chain
+    , module Plutus.StateMachine
+    , module Plutus.Utils
+    , module Plutus.Validation
+    ) where
+
+import Control.Monad
+import Optics
+
+import Plutus.Hash
+import Plutus.Types
+import Plutus.Chain
+import Plutus.StateMachine
+import Plutus.Utils
+import Plutus.Validation
